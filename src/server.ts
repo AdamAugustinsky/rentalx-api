bfdc282afe33462d77270af051aa5ee74715e194
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-16 20:00:06 -0300

    feat(createCategories)

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "teste"});
-});
+app.use(express.json());
 
-app.listen(3333, () => console.log("test..."));
+app.use(categoriesRoutes);
+
+app.listen(3333);
