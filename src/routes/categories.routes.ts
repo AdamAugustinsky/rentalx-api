716476add216dc603a6cd20fea73d8cfd4074dfe
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-16 20:35:23 -0300

    refactor(createCategory): uses Category entity

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from 'uuid';
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = new Category(
     name,
     description,
-    id: uuidV4()
-  })
+  )
+
+  categories.push(category);
 
   return response.status(201).send();
 })
