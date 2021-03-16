dff66defb7218f2be1b516ed9b857bcf53685553
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-16 20:48:19 -0300

    feat(categoriesRepository)

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description}: ICreateCategoryDTO): void {
+
+    const created_at = new Date();
+    const category = new Category(
+      name,
+      description,
+      created_at
+    )
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository }
