1b3b46afda5e31f92334fe6cbd2379ccc9e47794
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2021-03-17 21:26:39 -0300

    feat(specificationRepository)

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,30 @@
+import { Specification } from "../model/Specification";
+import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import { ISpecificationsRepository } from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specification: Specification[];
+
+  constructor() {
+    this.specification = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const created_at = new Date();
+    const specification = new Specification(
+      name,
+      description,
+      created_at
+    );
+
+    this.specification.push(specification);
+  }
+
+      findByName(name: string): Specification | undefined {
+    const specification = this.specification.find(specification => specification.name === name);
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository }
