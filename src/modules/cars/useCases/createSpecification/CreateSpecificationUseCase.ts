d74eceaaaeb104edf09dd4c566cffdd53814af89
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-03-25 22:05:17 -0300

    refactor(createSpecifications): use tsyringe for  depency injection

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,3 +1,4 @@
+import { inject, injectable } from "tsyringe";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface ICreateSpecification {
@@ -5,8 +6,12 @@ interface ICreateSpecification {
   description: string,
 }
 
+@injectable()
 class CreateSpecificationUseCase {
-  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  constructor(
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationsRepository
+    ) {}
   execute({ name, description }: ICreateSpecification) {
     const nameAlreayInUse = this.specificationsRepository.findByName(name);
 
