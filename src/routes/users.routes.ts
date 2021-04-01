1992b515c6ecaabce36347e6a17ffe0f0b8ff475
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2021-03-31 23:01:14 -0300

    feat(users.routes): add UploadUserAvatar to routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/cars/useCases/updateUserAvatar/UpdateUserAvatarController";
+import uploadConfig from "../config/upload";
+import multer from "multer";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/avatar",  
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"), 
+  updateUserAvatarController.handle);
 
 export { usersRoutes };
