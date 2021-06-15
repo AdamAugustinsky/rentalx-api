e4a5984775117803c2e9f6c50f7dd6602c0d5dc5
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-06-15 00:03:44 -0300

    feat(routes): use ensureIsAdmin middleware
    cars, categories and specifications are now using ensureIsAdmin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const carsRoutes = Router();
 
 let createCarController = new CreateCategoryController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post("/", ensureAuthenticated, ensureIsAdmin, createCarController.handle);
 
 export { carsRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationCotroller";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post("/", ensureAuthenticated, ensureIsAdmin, createSpecificationController.handle);
 
 export { specificationsRoutes };
