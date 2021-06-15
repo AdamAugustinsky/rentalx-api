96f774a8cb206895023f0630effef61f72e9738a
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-06-14 22:48:12 -0300

    feat(carsRepository): register as tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,11 @@
 import { container } from 'tsyringe';
 import { UsersRepository } from '../../modules/accounts/infra/typeorm/repositories/implementations/UsersRepository';
 import { IUsersRepository } from '../../modules/accounts/infra/typeorm/repositories/IUsersRepository';
-import { ICategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/implementations/CategoriesRepository';
-import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/ISpecificationsRepository';
+import { ICategoriesRepository } from '../../modules/cars/infra/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ISpecificationsRepository } from '../../modules/cars/infra/repositories/ISpecificationsRepository';
+import { CarsRepository } from '../../modules/cars/infra/typeorm/repositories/CarRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -19,4 +20,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+)
+
+container.registerSingleton<CarsRepository>(
+  "CarsRepository",
+  CarsRepository
 )
