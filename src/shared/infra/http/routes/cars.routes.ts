b12bf9ff68421d1ce3532fd5dbd526a7f965a4b6
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-06-16 18:03:17 -0300

    feat: add listAllAvailableCars to routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
+import { ListAllAvailableCarsController } from "../../../../modules/cars/useCases/listCars/ListAllAvailableCarsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const carsRoutes = Router();
 
 let createCarController = new CreateCategoryController();
+let listAllAvailableCarsController = new ListAllAvailableCarsController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureIsAdmin, createCarController.handle);
+
+carsRoutes.get("/available", listAllAvailableCarsController.handle);
 
 export { carsRoutes };
