85d5ab913493144079d0f85e3fdb68faecf518ca
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-06-16 19:18:42 -0300

    fix(carsRoutes): use the correct CreateCarController

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { CreateCategoryController } from "../../../../modules/cars/useCases/createCategory/CreateCategoryController";
+import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
 import { ListAllAvailableCarsController } from "../../../../modules/cars/useCases/listCars/ListAllAvailableCarsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureIsAdmin } from "../middlewares/ensureIsAdmin";
 
 const carsRoutes = Router();
 
-let createCarController = new CreateCategoryController();
+let createCarController = new CreateCarController();
 let listAllAvailableCarsController = new ListAllAvailableCarsController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureIsAdmin, createCarController.handle);
