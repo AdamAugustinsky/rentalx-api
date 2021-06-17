ff43ad70bbb38fe6cdb9342a2ea3956aab99244e
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-06-17 00:41:49 -0300

    feat(ICarsRepository): add findById

--- src/modules/cars/infra/repositories/ICarsRepository.ts
+++ src/modules/cars/infra/repositories/ICarsRepository.ts
@@ -5,6 +5,7 @@ interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>
   findByLicensePlate(license_plate: string): Promise<Car | undefined>
   findAllAvailable( category_id?: string, brand?: string, name?: string): Promise<Car[]>
+  findById(id: string): Promise<Car | undefined>
 }
 
 export { ICarsRepository }
